cbb909fd27f3cccc568577eb788f0ebdcbd47a5e
mis401 <62945435+mis401@users.noreply.github.com>
2021-09-26 19:22:58 +0200

    Settings to support working, TableLayout

---
+++ app/src/main/java/com/test/rnids/Support.kt
@@ -0,0 +1,12 @@
+package com.test.rnids
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Support : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_support)
+    }
+
+}
